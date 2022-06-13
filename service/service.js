2145629ceb54573e9b21589ae9f7f5ecc802e153
aabytt <84480313+aabytt@users.noreply.github.com>
2022-06-13 13:09:00 +0300

    Update service.js

--- service/service.js
+++ service/service.js
@@ -1,11 +1,21 @@
 var Service = require('webos-service');
-var service = new Service('com.lampa.tv.service');
-var ffprobe = require('ffprobe'),
-    ffprobeStatic = require('ffprobe-static');
+var service = new Service('com.lampa.tv.ffprobe.service');
+var exec = require('child_process').exec;
 
 service.register("ffprobe", function(message) {
-	ffprobe(message.payload.uri, { path: ffprobeStatic.path }, function (err, info) {
-	  if (err) return done(err);
-	  message.respond({returnValue: true, data: JSON.stringify(info)});
-	});
+	
+	exec('./ffprobe -v error -print_format json -show_entries stream=index,codec_type,codec_name,codec_long_name,codec_time_base,sample_rate,channels,channel_layout,bit_rate:stream_tags=title,handler_name,language "' + message.payload.uri + '"', (error, stdout, stderr) => {
+	  if (error) {
+		console.error(error.message);
+		message.respond({returnValue: true, result: '[ERROR]', data: '[DETAILS] ' + error});
+		return;
+	  }
+	  if (stderr) {
+		console.error(stderr);
+		message.respond({returnValue: true, result: '[STDERROR]', data: '[DETAILS] ' + stderr});
+		return;
+	  }
+		message.respond({returnValue: true, result: '[SUCCESS]', data: stdout});
+		console.log(stdout);
+	});	
 });

7bf14c3241ced5d0a1e55a9b30f49a54d183f072
aabytt <84480313+aabytt@users.noreply.github.com>
2022-06-14 16:26:46 +0300

    Update service.js

--- service/service.js
+++ service/service.js
@@ -1,21 +1,18 @@
 var Service = require('webos-service');
 var service = new Service('com.lampa.tv.ffprobe.service');
-var exec = require('child_process').exec;
+var exec = require('child_process').exec,
+    child;
 
 service.register("ffprobe", function(message) {
-	
-	exec('./ffprobe -v error -print_format json -show_entries stream=index,codec_type,codec_name,codec_long_name,codec_time_base,sample_rate,channels,channel_layout,bit_rate:stream_tags=title,handler_name,language "' + message.payload.uri + '"', (error, stdout, stderr) => {
-	  if (error) {
-		console.error(error.message);
-		message.respond({returnValue: true, result: '[ERROR]', data: '[DETAILS] ' + error});
-		return;
-	  }
-	  if (stderr) {
-		console.error(stderr);
-		message.respond({returnValue: true, result: '[STDERROR]', data: '[DETAILS] ' + stderr});
-		return;
-	  }
+	child = exec('./ffprobe -v error -print_format json -show_entries stream=index,codec_type,codec_name,codec_long_name,codec_time_base,sample_rate,channels,channel_layout,bit_rate:stream_tags=title,handler_name,language "' + message.payload.uri + '"',
+	  function (error, stdout, stderr) {
+		if (error !== null) {
+			message.respond({returnValue: true, result: '[ERROR]', data: '[DETAILS] ' + error});
+		}
+		if (stderr !== null) {
+			message.respond({returnValue: true, result: '[STDERROR]', data: '[DETAILS] ' + stderr});
+		}
+		
 		message.respond({returnValue: true, result: '[SUCCESS]', data: stdout});
-		console.log(stdout);
-	});	
+	});
 });

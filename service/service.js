8975ecf264ca80e85ac768d1ceb76213633d2d42
aabytt <84480313+aabytt@users.noreply.github.com>
2022-06-15 09:32:06 +0300

    Update service.js

--- service/service.js
+++ service/service.js
@@ -1,18 +1,15 @@
 var Service = require('webos-service');
-var service = new Service('com.lampa.tv.ffprobe.service');
+var service = new Service('com.lampa.tv.service');
 var exec = require('child_process').exec,
     child;
 
 service.register("ffprobe", function(message) {
 	child = exec('./ffprobe -v error -print_format json -show_entries stream=index,codec_type,codec_name,codec_long_name,codec_time_base,sample_rate,channels,channel_layout,bit_rate:stream_tags=title,handler_name,language "' + message.payload.uri + '"',
 	  function (error, stdout, stderr) {
+		message.respond({returnValue: true, result: '[FFPROBE OUTPUT]', data: stdout, stderrText: stderr});
+		
 		if (error !== null) {
 			message.respond({returnValue: true, result: '[ERROR]', data: '[DETAILS] ' + error});
-		}
-		if (stderr !== null) {
-			message.respond({returnValue: true, result: '[STDERROR]', data: '[DETAILS] ' + stderr});
-		}
-		
-		message.respond({returnValue: true, result: '[SUCCESS]', data: stdout});
+		}	
 	});
 });

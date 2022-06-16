d5f1af23449faac93a7046401712e98415b45df3
aabytt <84480313+aabytt@users.noreply.github.com>
2022-06-16 12:40:41 +0300

    Update service.js

--- service/service.js
+++ service/service.js
@@ -11,15 +11,13 @@ var exec = require('child_process').exec,
 service.register("ffprobe", function(message) {
 	parsedUri = url(message.payload.uri);
 	dns_resolve = dns(parsedUri.hostname, function onLookup(err, addresses, family) {
-		var options = '-v error -print_format json -show_entries stream=index,codec_type,codec_name,codec_long_name,codec_time_base,sample_rate,channels,channel_layout,bit_rate:stream_tags=title,handler_name,language';
-		var resolvedUri =  '"' + parsedUri.protocol + '//'+ addresses + (parsedUri.port ? (':' + parsedUri.port) : '') + parsedUri.path + '"';	
-		child = exec(`./ffprobe ${options} ${resolvedUri}`,
+		var resolvedUri = parsedUri.protocol + '//'+ addresses + (parsedUri.port ? (':' + parsedUri.port) : '') + parsedUri.path;	
+		child = exec('./ffprobe -v error -print_format json -show_entries stream=index,codec_type,codec_name,codec_long_name,codec_time_base,sample_rate,channels,channel_layout,bit_rate:stream_tags=title,handler_name,language ' + '"' + resolvedUri+'"',
 		  function (error, stdout, stderr) {
-			message.respond({returnValue: true, result: '[FFPROBE OUTPUT]', data: stdout, stderrText: stderr});
+			message.respond({returnValue: true, result: '[FFPROBE OUTPUT] Resolved uri: ' + resolvedUri, data: stdout, stderrText: stderr});
 			if (error !== null) {
 				message.respond({returnValue: true, result: '[ERROR]', data: '[DETAILS] ' + error});
 			}	
 		});
 	});
-
 });
